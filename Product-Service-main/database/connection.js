503b2514878266863c706ae950578e20a7e79fda
Edward Ayirebi Acquah <129085304+eddie-ny@users.noreply.github.com>
2025-02-09 22:37:59 +0000

    Delete Product-Service-main directory

--- Product-Service-main/database/connection.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: 'postgres',
-  logging: false
-});
-
-module.exports = sequelize;
